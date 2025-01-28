0e62e09383f971177acbbbf22512dee7b15e71c7
BlueTot <bluetotkc@gmail.com>
2025-01-28 17:37:11 +0000

    test cases working for ex1

--- src/CourseworkOne.hs
+++ src/CourseworkOne.hs
@@ -15,18 +15,27 @@ numOccurrences hand cardRank = length $ filter (\(Card r _) -> r == cardRank) ha
 
 -- Function to count the occurrences of each rank in a hand
 countRanks :: Hand -> [Int]
-countRanks hand = [numOccurrences hand r | r <- [Two ..]]
+countRanks hand = filter (> 0) [numOccurrences hand r | r <- [Two ..]]
 
--- Get number of instances of a given occurrence in the list of occurrences of a hand
-numRanks :: Hand -> Int -> Int
-numRanks hand count = length $ filter (==count) $ countRanks hand
+-- -- Get number of instances of a given occurrence in the list of occurrences of a hand
+-- numRanks :: Hand -> Int -> Int
+-- numRanks hand count = length $ filter (==count) $ countRanks hand
 
--- Function to get suites from a hand
-suites :: Hand -> [Suit]
-suites hand = toList $ fromList $ map (\(Card _ s) -> s) hand
+-- Check if there are at least k instances of a given occurrence or higher in the list of occurrences of a hand
+enoughRankCount :: Hand -> Int -> Int -> Bool
+enoughRankCount hand count target = length (filter (>=count) $ countRanks hand) == target
 
+-- Function to get unique suites from a hand
+uniqueSuites :: Hand -> [Suit]
+uniqueSuites hand = toList $ fromList $ map (\(Card _ s) -> s) hand
+
+-- Function to get ranks from a hand
 ranks :: Hand -> [Rank]
 ranks = map (\(Card r _) -> r)
+
+-- Function to get unique ranks from a hand
+uniqueRanks :: Hand -> [Rank]
+uniqueRanks hand = toList $ fromList (ranks hand)
 
 -- Function to check if a hand is ascending
 isAscending :: Hand -> Bool
@@ -38,20 +47,38 @@ isAscending hand =
             [2, 3, 4, 5, 11] -> True
             _ -> and [b - a == 1 | (a, b) <- zip scores (tail scores)]
 
+isStraight :: Hand -> Bool
+isStraight hand = 
+    let
+        handRanks = sort $ ranks hand
+    in
+        handRanks == [Two, Three, Four, Five, Ace] ||
+        handRanks == [Ten, Jack, Queen, King, Ace] || isAscending hand
+
+isHighCard :: Hand -> Bool
+isHighCard hand = not (isStraight hand) && length (uniqueRanks hand) == 5
+
+isFlush :: Hand -> Bool
+isFlush hand = length (uniqueSuites hand) == 1
+
+isRoyal :: Hand -> Bool
+isRoyal hand = sort (ranks hand) == [Ten, Jack, Queen, King, Ace]
+
 -- Main function for Exercise 1
 contains :: Hand -> HandType -> Bool
-contains hand handType = case handType of
-    None -> null hand
-    HighCard -> not $ null hand
-    Pair -> numRanks hand 2 >= 1
-    TwoPair -> numRanks hand 2 >= 2
-    ThreeOfAKind -> numRanks hand 3 >= 1
-    Straight -> isAscending hand
-    Flush -> length (suites hand) == 1
-    FullHouse -> numRanks hand 3 == 1 && numRanks hand 2 == 1
-    FourOfAKind -> numRanks hand 4 >= 1
-    StraightFlush -> isAscending hand && length (suites hand) == 1
-    RoyalFlush -> sort (ranks hand) == [Ace, King, Queen, Jack, Ten]
+contains hand handType
+    | handType == RoyalFlush && isRoyal hand && isFlush hand = True
+    | handType == StraightFlush && isStraight hand && isFlush hand = True
+    | handType == FourOfAKind && enoughRankCount hand 4 1 = True
+    | handType == FullHouse && sort (countRanks hand) == [2, 3] = True
+    | handType == Flush && isFlush hand = True
+    | handType == Straight && isStraight hand = True
+    | handType == ThreeOfAKind && enoughRankCount hand 3 1 = True
+    | handType == TwoPair && enoughRankCount hand 2 2 = True
+    | handType == Pair && enoughRankCount hand 2 1 = True
+    | handType == HighCard && isHighCard hand = True
+    | handType == None && null hand = True
+    | otherwise = False
 
 --------------------------------------------------------------------------------
 -- Part 2: identify the highest value hand type in a played hand
